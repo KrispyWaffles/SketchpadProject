0c9acbf5bb8d5fa7af80d07cc64bfb354475c038
KrispyWaffles <173952614+KrispyWaffles@users.noreply.github.com>
2024-10-11 23:35:17 -0700

    was able to create a grid

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-let userInput = 0;
-let gridRow = 0;
-let cell = 0;
+// let userInput = 0;
+const pad = document.querySelector(".sketchContainer");
 
-function enterBoxNum() {
-if (userInput < 2 || userInput > 100) {
-    document.getElementById("gridSize").textContent = "Invalid Choice";
+
+
+for (let i = 1; i < 257; i++) {
+    const div = document.createElement('div');
+    div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+    pad.appendChild(div);
+}
+
+// function enterBoxNum() {
+// if (userInput < 2 || userInput > 100) {
+//     document.getElementById("gridSize").textContent = "Invalid Choice";
     
-    }
-    userSketch();
-}
+//     }
+//     userSketch();
+// }
 
-function userSketch() {
-    for (let i = 0; i < x; i++) {
-        gridRow.createElement(div);
-        gridRow.classList.add("row")
-    }
-    for (let i = 0; i < x; i++) {
-        cell.createElement(div)
-        cell.classList.add("cell")
-        gridRow.appendItem(cell)
-    }
-    container.appendItem(row)
-}
