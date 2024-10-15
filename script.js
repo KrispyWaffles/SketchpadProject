0ea076123204915a04da61e57dd61f49edb5bcf0
KrispyWaffles <173952614+KrispyWaffles@users.noreply.github.com>
2024-10-15 15:41:06 -0700

    trying to figure out event listener and getting the grid back

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 // let userInput = 0;
 const pad = document.querySelector(".sketchContainer");
 
+const button = document.getElementById('startButton');
+
+let userInput = document.getElementById("gridSize")
+
+let x = userInput.value;
+
+button.addEventListener('click', function() {
+    console.log(userInput)
+ startSketch();
+});
 
 
-for (let i = 1; i < 257; i++) {
-    const div = document.createElement('div');
-    div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-    pad.appendChild(div);
+function startSketch() {
+if (x > 100) {
+    document.getElementById("appInfo").textContent = "Size can't exceed 100!";
+} else {
+
+    for (let i = 0; i < x; i++) {
+        let row = document.createElement('div')
+        row.classList.add("row")
+    
+    
+    for (let i = 0; i < x; i++) {
+        let cell = document.createElement('div')
+        cell.classList.add("cell")
+        row.appendChild(cell)
+    }
+    pad.appendChild(row)
+
+
+}
 }
 
+}
 // function enterBoxNum() {
 // if (userInput < 2 || userInput > 100) {
 //     document.getElementById("gridSize").textContent = "Invalid Choice";
