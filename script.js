0e139ff250d8e80a2c2a37d29fff2351ab616230
KrispyWaffles <173952614+KrispyWaffles@users.noreply.github.com>
2024-10-15 18:23:07 -0700

    finally got the grid to display based on user input

--- script.js
+++ script.js
@@ -1,45 +1,42 @@
 // let userInput = 0;
 const pad = document.querySelector(".sketchContainer");
 
-const button = document.getElementById('startButton');
+const button = document.getElementById("startButton");
 
-let userInput = document.getElementById("gridSize")
+let userInput = document.getElementById("gridSize");
 
-let x = userInput.value;
 
-button.addEventListener('click', function() {
-    console.log(userInput)
- startSketch();
+
+
+button.addEventListener("click", function () {
+  let x = userInput.value;
+  console.log(x)
+  if (x > 100) {
+    document.getElementById("appInfo").textContent = "Size can't exceed 100!";
+  } else {
+    clearGrid();
+    startSketch(x);
+    console.log(x);
+  }
 });
 
+function clearGrid() {
+  pad.innerHTML = "";
+}
 
-function startSketch() {
-if (x > 100) {
-    document.getElementById("appInfo").textContent = "Size can't exceed 100!";
-} else {
+function startSketch(x) {
+  console.log(x);
+  for (let i = 0; i < x; i++) {
+      let row = document.createElement("div");
+      row.classList.add("row");
 
-    for (let i = 0; i < x; i++) {
-        let row = document.createElement('div')
-        row.classList.add("row")
-    
-    
-    for (let i = 0; i < x; i++) {
-        let cell = document.createElement('div')
-        cell.classList.add("cell")
-        row.appendChild(cell)
+      for (let i = 0; i < x; i++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        row.appendChild(cell);
+      }
+      pad.appendChild(row);
     }
-    pad.appendChild(row)
+  }
 
-
-}
-}
-
-}
-// function enterBoxNum() {
-// if (userInput < 2 || userInput > 100) {
-//     document.getElementById("gridSize").textContent = "Invalid Choice";
-    
-//     }
-//     userSketch();
-// }
 
